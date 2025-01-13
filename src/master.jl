d40979e0387148d7ad8518daa0519900ec6567e2
hareruya3303 <hareruya@protonmail.com>
2025-01-14 01:58:59 +0900

    Added master file
    Master file invoking all dependencies and initializing the system.

---
+++ src/master.jl
@@ -0,0 +1,24 @@
+module ISPstitcher
+dir = @__DIR__
+using FFTW, Images, LinearAlgebra, Statistics, DSP, Suppressor, ArrayAllocators, Match
+using FFTW: rFFTWPlan
+import Base: *
+
+GPUArray, GPUVector, GPUMatrix = try begin using CUDA
+        @suppress_out CUDA.device()
+        println("\nCUDA GPU detected! GPU option for acceleration will default to this! Set gpu=true option to use it!")
+    end
+    CuArray, CuVector, CuMatrix
+catch e
+    try begin using Metal
+            @suppress_out Metal.versioninfo()
+            println("\nMetal GPU detected! Unfortunately, nothing is set up to work with it yet!")
+        end
+        MtlArray, MtlVector, MtlMatrix
+    catch em
+        Array, Vector, Matrix
+    end
+end;
+
+include(string(dir, "/imgconv/imgconv.jl"))
+end
